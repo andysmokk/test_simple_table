acdedfb9d3bc138b67ad0ac60f54b68f1fe87446
Andrii Konovalov <andy.smokk@gmail.com>
2024-08-31 00:55:21 +0200

    finished6

--- app/page.js
+++ app/page.js
@@ -1,20 +1,19 @@
-import React, { Suspense } from "react";
+import React, { Suspense, lazy } from "react";
 
-import Header from "@/components/Header";
-import MainTable from "@/components/MainTable";
+// import Header from "@/components/Header";
+// import MainTable from "@/components/MainTable";
 import LoaderSpinner from "@/components/LoaderSpinner";
+
+const Header = lazy(() => import("@/components/Header"));
+const MainTable = lazy(() => import("@/components/MainTable"));
 
 export default function Home() {
   return (
     <>
-      <Suspense
-        fallback={
-          <div className="flex-center w-full">
-            <LoaderSpinner size={40} />
-          </div>
-        }
-      >
+      <Suspense fallback={<LoaderSpinner size={20} />}>
         <Header />
+      </Suspense>
+      <Suspense fallback={<LoaderSpinner size={40} />}>
         <main>
           <MainTable />
         </main>
