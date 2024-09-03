9c9a69015bdea89399b73cba592ebc4b2ea08e9f
Andrii Konovalov <andy.smokk@gmail.com>
2024-09-03 16:17:13 +0200

    Combobox component optimized

--- components/Combobox.jsx
+++ components/Combobox.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Image from "next/image";
-import { useEffect, useState } from "react";
+import { useMemo, useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 import { CheckIcon } from "@radix-ui/react-icons";
 
@@ -22,20 +22,16 @@ import { numberOfRows } from "@/constants/index";
 
 const Combobox = () => {
   const [open, setOpen] = useState(false);
-  const [value, setValue] = useState("");
   const searchParams = useSearchParams();
   const router = useRouter();
 
-  useEffect(() => {
-    const itemsCount = searchParams.get("itemsPerPage") || "10";
-    setValue(itemsCount);
-  }, [searchParams]);
+  const value = searchParams.get("itemsPerPage") || "10";
 
-  const handleSelect = (currentValue) => {
-    const newValue = currentValue === value ? "" : currentValue;
-    setValue(newValue);
-    setOpen(false);
+  const selectedLabel = useMemo(() => {
+    return numberOfRows.find((number) => number.value === value)?.label || "10";
+  }, [value]);
 
+  const handleSelect = (newValue) => {
     const searchQuery = searchParams.get("search") || "";
 
     if (searchQuery) {
@@ -43,6 +39,8 @@ const Combobox = () => {
     } else {
       router.push(`/?itemsPerPage=${newValue}`);
     }
+
+    setOpen(false);
   };
 
   return (
@@ -51,13 +49,11 @@ const Combobox = () => {
         <Button
           variant="outline"
           role="combobox"
-          aria-expanded={open}
+          aria-expanded={true}
           className="w-[55px] gap-1 bg-white-2 py-2 px-2 text-black
            font-medium dark:bg-bg-dark dark:text-text-dark dark:hover:bg-accent dark:focus:bg-accent"
         >
-          {value
-            ? numberOfRows.find((number) => number.value === value)?.label
-            : "10"}
+          {selectedLabel}
           <Image
             src="/icons/caret-down.svg"
             alt="caret-down"
